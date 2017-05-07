e7f479931727f60c4814c738347199f517dab939
masterkey94 <masterkey94@users.noreply.github.com>
2017-05-06 19:41:46 -0700

    Add files via upload

---
+++ server.R
@@ -0,0 +1,65 @@
+# Server.R code file for MPG Predictor using Motor Trend Car Road Tests Data
+# Objective of the app is to predict a car's miles per gallon performance based 
+# on the transmission type, weight, and horsepower of the car
+
+# UI.R will enable user to input the following:
+# - transmission type: automatic or manual
+# - weight of car: 1000lbs to 6000lbs
+# - horsepower of car: 50hp to 500hp
+
+# Output of the will be the following:
+# - predicted miles per gallon performance based on the inputs
+# - graph of mpg performance vs. weight for predicted value and mtcars data
+# - graph of mpg performance vs. horsepoer for predicted value and mtcars data
+
+# Load needed libraries
+library(shiny)
+library(caret)
+library(ggplot2)
+
+data(mtcars)
+
+# Fit a linear model on mtcars with am, hp, and wt as predictors
+modelfit <- lm(mpg ~ am + hp + wt, data = mtcars)
+
+# Define server logic required to predict mpg
+shinyServer(function(input, output) {
+
+    # create data frame with user inputted values for transmission, horsepower, weight from UI
+    Data = reactive({
+      newmpgdata <- data.frame(am = as.numeric(input$Transmission), hp = input$HP, wt = input$Weight/1000)
+        return(list(newmpgdata = newmpgdata))
+    })
+    
+    # create plot data
+    plotdata <- data.frame(type = "mtcars Data", hp = mtcars$hp, wt = mtcars$wt, mpg = mtcars$mpg)
+    Update = reactive({
+      newrow = data.frame(type = "Prediction", 
+                          hp = input$HP, 
+                          wt = input$Weight/1000, 
+                          mpg = predict(modelfit, Data()$newmpgdata))
+      plotdata = rbind(plotdata, newrow)
+      return(list(plotdata = plotdata))
+    })
+    
+    # output predicted mpg
+    output$Transmission <- renderText(round(predict(modelfit, Data()$newmpgdata),2))
+
+    # output plot of weight vs. mpg
+    output$Weightplot<- renderPlot({
+      p <- ggplot(Update()$plotdata, aes(wt, mpg)) 
+      p <- p + geom_point(aes(color = as.factor(type)), size = 5)
+      p <- p + labs(x = "Weight ('000s lbs.)", y = "Miles Per Gallon", color = "Data Type")
+      p <- p + geom_smooth(method = "lm")
+      print(p)
+    })
+
+    # output plot of hp vs. mpg
+    output$HPplot<- renderPlot({
+      p <- ggplot(Update()$plotdata, aes(hp, mpg)) + geom_point(aes(color = as.factor(type)), size = 5)
+      p <- p + labs(x = "Horsepower", y = "Miles Per Gallon", color = "Data Type")
+      p <- p + geom_smooth(method = "lm")
+      print(p)
+    })    
+    
+})
---
+++ ui.R
@@ -0,0 +1,56 @@
+# UI.R code file for MPG Predictor using Motor Trend Car Road Tests Data
+# Objective of the app is to predict a car's miles per gallon performance based 
+# on the transmission type, weight, and horsepower of the car
+
+# UI.R will enable user to input the following:
+# - transmission type: automatic or manual
+# - weight of car: 1000lbs to 6000lbs
+# - horsepower of car: 50hp to 500hp
+
+# Output of the will be the following:
+# - predicted miles per gallon performance based on the inputs
+# - graph of mpg performance vs. weight for predicted value and mtcars data
+# - graph of mpg performance vs. horsepoer for predicted value and mtcars data
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  titlePanel("MPG Predictor using Motor Trend Car Road Tests Data"),
+  
+  # Sidebar with inputs for transmission type, horsepower, and weight of car 
+  sidebarPanel(
+    h4("Please select the values for the transmission type, horsepower, and weight of the car below."),
+    radioButtons("Transmission", "Transmission Type:",
+                 c("Automatic" = 0, "Manual" = 1), 
+                 selected = 0),
+    sliderInput("HP",
+                 "Horsepower of Car:",
+                 min = 50,
+                 max = 500,
+                   value = 300),
+    sliderInput("Weight",
+                 "Weight of Car (lbs.):",
+                 min = 1000,
+                 max = 6000,
+                 value = 3000),
+    submitButton("Submit")
+  ),
+    
+    # Output predicted mpg and plots
+    mainPanel(
+      h4("This tool enables you to predict the miles per gallon based on the transmission type, 
+        horsepower, and weight of a car based on the data from the Motor Trend Card Road Test 
+        data set 'mtcars'.  Then you can compare the resulting predicted mpg versus mpg data from 
+        the 'mtcars' data set by horsepower and weight."),
+      h3("Predicted miles per gallon: "),
+      h3(uiOutput('Transmission')),
+      h3("Plot of Miles Per Gallon vs. Horsepower"),
+      plotOutput("HPplot"),
+      h3("Plot of Miles Per Gallon vs. Weight"),
+      plotOutput("Weightplot")
+
+    )
+))
